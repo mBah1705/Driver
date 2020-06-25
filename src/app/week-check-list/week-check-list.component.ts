19d47c2e9da6aa3e4d62e065c2adbc4c6aa644cf
Mamadou Bah <mamadou.bah-ext@groupeseloger.com>
2020-06-25 20:50:03 +0200

    Slide toggle get outer label (not inner because we cant restyle that one)

--- src/app/week-check-list/week-check-list.component.ts
+++ src/app/week-check-list/week-check-list.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeekCheckListComponent implements OnInit {
   checks = [
-    {id: 1, label: 'Pare-brises avant et arrière'},
-    {id: 2, label: 'Rétroviseurs externes et internes'},
-    {id: 3, label: 'Essuie-glaces avants et arrières'},
-    {id: 4, label: 'Liquide lave-glace'},
-    {id: 5, label: 'Slide me!'},
-    {id: 6, label: 'Slide me!'},
-    {id: 7, label: 'Slide me!'},
-    {id: 8, label: 'Slide me!'},
+    {label: 'Pare-brises avant et arrière'},
+    {label: 'Rétroviseurs externes et internes'},
+    {label: 'Essuie-glaces avants et arrières'},
+    {label: 'Liquide lave-glace'},
+    {label: 'Slide me!'},
+    {label: 'Slide me!'},
+    {label: 'Slide me!'},
+    {label: 'Slide me!'},
   ];
 
   constructor() { }
