2335baa56691da1e68659c89b2b48bf2a60e9deb
Mamadou Bah <mamadou.bah-ext@groupeseloger.com>
2020-06-25 21:50:11 +0200

    Toggle sidenav on click item if isHandset

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,13 +8,12 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  mainCardLabel = 'Activité du mois';
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: this.mainCardLabel, cols: 1, rows: 1 },
+          { title: 'Mois', cols: 1, rows: 1 },
           { title: 'Card 2', cols: 1, rows: 1 },
           { title: 'Card 3', cols: 1, rows: 1 },
           { title: 'Card 4', cols: 1, rows: 1 }
@@ -22,7 +21,7 @@ export class DashboardComponent {
       }
 
       return [
-        { title: this.mainCardLabel, cols: 2, rows: 1 },
+        { title: 'Activité du mois', cols: 2, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
         { title: 'Card 3', cols: 1, rows: 2 },
         { title: 'Card 4', cols: 1, rows: 1 }
